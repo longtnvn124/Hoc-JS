725e0d8f8e2f4333b592ac58a8a401d88b44770a
longtnvn124 <longkakainfo@gmail.com>
2022-06-20 21:10:49 +0700

    fist commit

---
+++ code 1/menu.js
@@ -0,0 +1,66 @@
+
+var fullName = 'Trần Minh Long';
+var age = 22;
+
+// alert(fullName);
+// alert(age);
+
+// console.log(fullName);
+
+// confirm('xác nhận bạn đã đủ tu');
+// prompt('xác nhận bạn đã đủ tuổi');
+
+// settimeout(function() {
+//     alert('thông báo');
+// }, 1000)
+
+// setInterval(function() {
+//     console.log('day la long ' + Math.random());
+// },1000)
+
+// var message = 'Học về built-in finctions trong JS';
+// var warMessage = 'đây là message cảnh báo';
+// var errorMessage = 'Đây là message lỗi';
+
+// console.log(message);
+// console.warn(warMessage);
+// console.error(errorMessage);
+
+//  toán tử ++ --
+
+// var a= 6;
+// // việc 1: a copy , a copy bằng 6
+// // việc 2: từ 1 
+//  var output = a--;
+//  console.log("output: ",output);
+//  console.log("a: ",a);
+
+// --------------------Array-------------------
+
+// cach1
+//  var languages= [
+//     'javaScript',
+//     'javaScript',
+//     'javaScript',
+//     null,
+//     undefined,
+//     function(){},
+//     123
+
+//  ];
+//  console.log(languages);
+
+
+// cach 2
+// var languages = new Array(
+//     'javaScript',
+//     'javaScript',
+//     'javaScript',
+   
+// );
+
+// // console.log(Array.isArray(new Array(1,2)));
+// console.log(languages.length);
+// console.log(languages[2])
+
+//------------------------------------------------------
